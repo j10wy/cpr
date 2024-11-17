b043026d6dbb03945e0fd12096ee33d31a83540b
Jeff Lowy <jeffreylowy@gmail.com>
2024-11-17 11:46:47 -0800

    Async player

--- src/components/podcast-episode.tsx
+++ src/components/podcast-episode.tsx
@@ -240,7 +240,7 @@ export function PodcastEpisodeComponent() {
         </div>
       </footer>
       <script
-        defer
+        async
         src="https://www.buzzsprout.com/2425181/episodes/16121975-shhh-the-library-s-closed-tales-from-a-district-that-knows-best.js?container_id=buzzsprout-player-16121975&player=small"
         type="text/javascript"
       />
