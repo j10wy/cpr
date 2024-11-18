a2797166c09c5fff138c7948a63c84c25b328a56
Jeff Lowy <jeffreylowy@gmail.com>
2024-11-17 23:55:25 -0800

    [TOC] Kinda working

---
+++ src/components/articles/toc.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import * as React from "react";
+import { useEffect, useRef, useState } from "react";
+import { ChevronUp } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+interface TableOfContentsItem {
+  id: string;
+  title: string;
+  level: number;
+}
+
+// THIS IS BROKEN BECAUSE THE HEADINGS DO NOT HAVE IDS.
+
+export default function ToC() {
+  const [activeId, setActiveId] = useState<string>("");
+  const [toc, setToc] = useState<TableOfContentsItem[]>([]);
+  const observerRef = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    const doc = document.getElementById("article");
+    const headings = doc?.querySelectorAll<HTMLHeadingElement>("h2, h3, h4");
+    if (!headings) return;
+
+    for (const heading of headings) {
+      heading.id = crypto.getRandomValues(new Uint32Array(1))[0].toString();
+    }
+
+    const tocItems: TableOfContentsItem[] = Array.from(headings).map(
+      (heading) => ({
+        id: heading.id,
+        title: heading.textContent || "",
+        level: parseInt(heading.tagName.charAt(1)),
+      })
+    );
+    setToc(tocItems);
+
+    observerRef.current = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveId(entry.target.id);
+          }
+        });
+      },
+      { rootMargin: "0px 0px -80% 0px" }
+    );
+
+    headings.forEach((heading) => observerRef.current?.observe(heading));
+
+    return () => observerRef.current?.disconnect();
+  }, []);
+
+  const handleTOCClick = (id: string) => {
+    console.log(id);
+    const element = document.getElementById(id);
+    console.log(element);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  if (toc.length === 0) return <></>;
+
+  return (
+    <>
+      <aside className="w-full">
+        <h2 className="text-xl font-semibold mb-4">Table of Contents</h2>
+        <ScrollArea>
+          <nav>
+            <ul className="space-y-1">
+              {toc.map((item) => (
+                <li key={item.id}>
+                  <Button
+                    variant="ghost"
+                    className={cn(
+                      "text-left"
+                      // activeId === item.id
+                      //   ? "bg-accent text-accent-foreground"
+                      //   : ""
+                    )}
+                    onClick={() => handleTOCClick(item.id)}
+                  >
+                    {item.title.length > 50
+                      ? `${item.title.substring(0, 40)}...`
+                      : item.title}
+                  </Button>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </ScrollArea>
+      </aside>
+      <Button
+        className="fixed bottom-4 right-4 rounded-full p-2"
+        size="icon"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        aria-label="Scroll to top"
+      >
+        <ChevronUp className="h-4 w-4" />
+      </Button>
+    </>
+  );
+}
--- src/components/post.tsx
+++ src/components/post.tsx
@@ -3,6 +3,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import Link from "next/link";
 import { PodcastPlayer } from "@/components/podcast/podcast-player";
 import { Article } from "@/components/article";
+import ToC from "@/components/articles/toc";
 
 export function Post() {
   return (
@@ -23,42 +24,46 @@ export function Post() {
               <CardContent>
                 <PodcastPlayer />
                 <Article>
-                  <h3 className="text-2xl font-extrabold tracking-tight">
-                    Empty Shelves, Empty Promises? Mount Diablo&apos;s Library
-                    Closures Leave Students in the Dark.
-                  </h3>
-
-                  <ul className="list-disc pl-6">
-                    <li>
-                      Developing robust ethical guidelines for AI development
-                      and deployment
-                    </li>
-                    <li>Investing in AI education and reskilling programs</li>
-                    <li>
-                      Encouraging interdisciplinary collaboration in AI research
-                    </li>
-                    <li>Implementing transparent and accountable AI systems</li>
-                    <li>
-                      Fostering public dialogue and understanding of AI
-                      technologies
-                    </li>
-                  </ul>
-
-                  <Card className="p-6 my-8 bg-primary/5 border-primary/10">
-                    <h3 className="text-lg font-semibold mb-2">
-                      Did you know?
+                  <div id="article">
+                    <h3 className="text-2xl font-extrabold tracking-tight">
+                      Empty Shelves, Empty Promises? Mount Diablo&apos;s Library
+                      Closures Leave Students in the Dark.
                     </h3>
-                    <p className="text-sm text-muted-foreground">
-                      According to a recent study by PwC, AI could contribute up
-                      to $15.7 trillion to the global economy by 2030, more than
-                      the current output of China and India combined.
-                    </p>
-                  </Card>
+
+                    <ul className="list-disc pl-6">
+                      <li>
+                        Developing robust ethical guidelines for AI development
+                        and deployment
+                      </li>
+                      <li>Investing in AI education and reskilling programs</li>
+                      <li>
+                        Encouraging interdisciplinary collaboration in AI
+                        research
+                      </li>
+                      <li>
+                        Implementing transparent and accountable AI systems
+                      </li>
+                      <li>
+                        Fostering public dialogue and understanding of AI
+                        technologies
+                      </li>
+                    </ul>
+
+                    <Card className="p-6 my-8 bg-primary/5 border-primary/10">
+                      <h3 className="text-lg font-semibold mb-2">
+                        Did you know?
+                      </h3>
+                      <p className="text-sm text-muted-foreground">
+                        According to a recent study by PwC, AI could contribute
+                        up to $15.7 trillion to the global economy by 2030, more
+                        than the current output of China and India combined.
+                      </p>
+                    </Card>
+                  </div>
                 </Article>
               </CardContent>
             </Card>
           </div>
-
           <Card>
             <CardHeader>
               <CardTitle>Sources and Additional Resources</CardTitle>
@@ -98,6 +103,7 @@ export function Post() {
                   </li>
                 </ul>
               </ScrollArea>
+              <ToC />
             </CardContent>
           </Card>
         </div>
